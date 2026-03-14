be51ea84bc476908fe7e564fda517f13d91bbe4a
Rajendra singh <rawatrajendrasingh33185@gmail.com>
2026-03-14 22:08:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+document.querySelectorAll(".join-btn").forEach(btn=>{
+btn.addEventListener("click",()=>{
+console.log("Join button clicked");
+});
+});
+
+});
